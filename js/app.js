c3a424df797d55402eb7a9e9c31d8e48b77afa9f
otknoy <otknoy@gmail.com>
2015-03-05 17:33:38 +0900

    Modify encount process

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
+var field_bgm = new Audio("sound/field.mp3");
+field_bgm.load();
+field_bgm.volume = 0.7;
+field_bgm.loop = true;
+field_bgm.play();
+
 var lat = 34.705895;
 var lng = 135.494474;
 var map = Map.createMap('map-canvas', lat, lng, 16);
-var marker = Map.createMarker(map, '現在地', lat, lng, true);
+var playerMarker = Map.createMarker(map, '現在地', lat, lng, true);
 
 Data.loadOsakaData().then(function(data) {
     var circles = [];
+    var samples = [];
 
     var updateCircles = function() {
 	var bounds = Map.getMapBounds(map);
@@ -14,7 +21,7 @@ Data.loadOsakaData().then(function(data) {
 					  bounds.lat2, bounds.lng2);
 
 	var cir_num = 12;
-	var samples = Data.sample(filters, cir_num);
+	samples = Data.sample(filters, cir_num);
         
 	for (var i = 0; i < samples.length; i++) {
 	    var d = samples[i];
@@ -43,11 +50,71 @@ Data.loadOsakaData().then(function(data) {
 
 	updateCircles();
     });
-    
-    // マーカーのドロップ（ドラッグ終了）時のイベント
-    google.maps.event.addListener(marker, 'dragend', function(ev){
-	// イベントの引数evの、プロパティ.latLngが緯度経度。
-        user_pos = ev.latLng;
+
+
+    // encout
+    $("img.deru").click(function() {
+	// sound
+	field_bgm.pause();
+	var buttonSound = new Audio("sound/deru_button.mp3");
+	buttonSound.load();
+	buttonSound.play();
+
+	var items = samples;
+	var playerPos = playerMarker.getPosition();
+	var placeType = checkPlaceType(items, circles, playerPos);
+
+	var enemyIndexes = getEncountingEnemyIndexes(placeType);
+	var enemyId = enemyIndexes[Math.floor(Math.random() * enemyIndexes.length)];
+
+	localStorage.setItem("current", enemyId);
+	location.href = "battle.html";
     });
 });
 
+function checkPlaceType(items, circles,  playerPos) {
+    var i = findNearestItemIndex(items, playerPos);
+    var item = items[i];
+    var circle = circles[i];
+    
+    var itemPos = new google.maps.LatLng(item.latitude, item.longitude);
+    var dist = google.maps.geometry.spherical.computeDistanceBetween(playerPos, itemPos);
+
+    var placeType = null;
+    if (dist < circle.getRadius()) {
+	placeType = item.type;
+    }
+
+    return placeType;
+}
+
+function findNearestItemIndex(items, playerPos) {
+    var index = null;
+    var minDist = Number.MAX_VALUE;
+    for (var i = 0; i < items.length; i++) {
+	var item = items[i];
+
+	var itemPos = new google.maps.LatLng(item.latitude, item.longitude);
+	var dist = google.maps.geometry.spherical.computeDistanceBetween(playerPos, itemPos);
+
+	if (dist < minDist) {
+	    minDist = dist;
+	    index = i;
+	}
+    }
+    return index;
+}
+
+function getEncountingEnemyIndexes(playceType) {
+    var skillType = "normal";
+    if (playceType != null) {
+	skillType = Data.place2skill[playceType];
+    }
+    
+    return {
+	"身体的パワー": [3, 4, 5, 6, 7, 8, 9, 10, 11],
+	"社会的パワー": [15, 16, 17, 18, 19, 20, 21, 22, 23],
+	"精神的パワー": [27, 28, 29,30,31,32,33,34,35],
+	"normal": [0, 1, 2, 12, 13, 14, 24, 25, 26]
+    }[skillType];
+}
--- js/tab.js
+++ js/tab.js
@@ -1,14 +1,7 @@
-
-var field_bgm = new Audio("sound/field.mp3");
-
 // 最初の状態
 $(".contents_frameA").show();
 $(".contents_frameB").hide();
 $(".contents_frameC").hide();
-	field_bgm.load();
-	field_bgm.volume = 0.7;
-	field_bgm.loop = true;
-	field_bgm.play();
 
 // ボタンをクリックしたときのイベント処理
 $(".tab-buttonA").on("click", function() {
